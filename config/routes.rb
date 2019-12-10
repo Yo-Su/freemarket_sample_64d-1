5bbeac4931b6ee0712d8785f02e46f70f2859e34
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-12-10 13:12:10 +0900

    fix Sider修正 Route.rbの記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :users, only: :show
-  resources :items, only: [:index, :show]
+  resources :items, only: [:show]
 end
