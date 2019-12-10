fcc107b1452eeb93f0d064f36e7e8cc0d64b4ca6
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-12-10 14:54:15 +0900

    add ログアウトページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  get 'users/logout', to: "users#logout"
   resources :users, only: :show
   resources :items, only: :show
 end
