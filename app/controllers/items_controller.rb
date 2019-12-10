0ac0d7dce2ab7e12b957730d7b472bbee1b89859
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-12-10 21:32:17 +0900

    add 商品DBから商品データを反映

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,8 @@ class ItemsController < ApplicationController
   end
 
   def show
-    binding.pry
     @item = Item.find(params[:id])
     @image = Image.includes(:item)
+    # binding.pry
   end
 end
