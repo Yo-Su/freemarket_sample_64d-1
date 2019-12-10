14aa08d8f10f83957d0f89b885206ffcd5c0b44e
natsuki-tacica <sns.senyou.n@gmail.com>
2019-12-10 20:33:54 +0900

    clean　コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  root "items#index"
+  devise_for :users
+  root to: "items#index"
+  resources :users, only: :show do
+    collection do
+      get 'logout'
+    end
+  end
+  resources :items, only: :show
 end
