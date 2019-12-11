1738884c9cc18623c434fdbbacbc141f202c77e7
kodaikodai <k.sagawaa@gmail.com>
2019-12-11 20:40:18 +0900

    update リファクタリングしました

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
-  has_many  :images,dependent: :destroy
-  has_many  :messages,dependent: :destroy
-  has_many  :likes,dependent: :destroy
-  belongs_to :user
-  belongs_to :brand
-  belongs_to :category
+  has_many    :images    ,dependent: :destroy
+  has_many    :messages  ,dependent: :destroy
+  has_many    :likes     ,dependent: :destroy
+  belongs_to  :user
+  belongs_to  :brand
+  belongs_to  :category
 end
