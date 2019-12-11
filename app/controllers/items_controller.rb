179dc4b2fd9570591fdc4fe7008f86d77891af9c
kodaikodai <k.sagawaa@gmail.com>
2019-12-11 21:05:59 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,22 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:status,:name,:from_delivery_area,:to_delivery_area,:price,:delivery_date,:size,:grade,:transfer_fee,:delivery_type,:describe,:buyer_id,:user_id,:brand_id,:category_id)
+    params.require(:item).permit(
+      :status,
+      :name,
+      :from_delivery_area,
+      :to_delivery_area,
+      :price,
+      :delivery_date,
+      :size,:grade,
+      :transfer_fee,
+      :delivery_type,
+      :describe,
+      :buyer_id,
+      :user_id,
+      :brand_id,
+      :category_id
+    )
   end
 
 end
