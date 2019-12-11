48219869940b9169dc6cf19ae3ea9aae08a54310
tsuchida031025 <tuusan33@gmail.com>
2019-12-11 18:47:40 +0900

    Revert "add 出品機能の追加（controllerの修正）"
    This reverts commit d22643687b8991baaace5d92a32528e6e27768af.

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -1,8 +1,12 @@
 class SellController < ApplicationController
-  before_action :create_items_instance, only:[:index,:destroy]
+  before_action :create_items_instance, only:[:index, :new,:show,:destroy]
 
   def index
     
+  end
+
+  def new
+    @item_image = @item.images.build
   end
 end
 
