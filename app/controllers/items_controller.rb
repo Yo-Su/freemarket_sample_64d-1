f9e15bf404da313c0222f3f0016671965b3f114a
natsuki-tacica <sns.senyou.n@gmail.com>
2019-12-15 20:17:04 +0900

    add コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create,:destroy,:edit,:update]
   before_action :get_item, only: [:show, :buy, :pay, :destroy]
-
-  require 'payjp'
-
   def index
   end
 
@@ -13,16 +10,9 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-#下記の記載は動作確認用のため本実装の際は削除する
-# @item.user_id = 1
-# @item.brand_id = 1
-# @item.category_id = 1
-    @item.user_id = 1
-    @item.brand_id = 1
-    @item.category_id = 1
     if @item.save
     else
-      redirect_to root_path
+      redirect_to root_path   
     end
   end
 
@@ -37,7 +27,8 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    if @item.destroy
+    @item.destroy
+    if @item.user_id == current_user.id
       redirect_to items_path,notice: '商品を削除しました'
     else
       redirect_to root_path
@@ -46,33 +37,14 @@ class ItemsController < ApplicationController
 
   def buy
     @image = Image.includes(:item).first
-    # ログインしているユーザーのカード情報を取得(ログイン機能が実装されていないため暫定で１を代入)
-    @card = Card.find_by(user_id: 1)
   end
 
   def pay
-    # クレジットカード決済===================================================================
-    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
-    # ログインしているユーザーのカード情報を取得(ログイン機能が実装されていないため暫定で１を代入)
-    card = Card.where(user_id: 1).first
-    if Payjp::Charge.create(
-        amount: @item.price,
-        customer: card.customer_id,
-        currency: 'jpy'
-      )
-    else
-      # 決済に失敗したらトップページに移動する
-      redirect_to root_path
-    end
-    # クレジットカード決済 ここまで============================================================
-
-    # 商品情報更新(出品状態変更・購入者ID追加)===================================================
     if @item.update(item_buy_params)
       redirect_to checkout_item_path
     else
       redirect_to root_path
     end
-    # 商品情報更新 ここまで===================================================================
   end
 
   def checkout
@@ -87,7 +59,7 @@ class ItemsController < ApplicationController
   def item_buy_params
     # current_user.idの代わり、ログイン機能実装後に入れ替える
     test_id = 2
-    params.permit(:id).merge(buyer_id: test_id, status: 1)
+    params.permit(:id).merge(buyer_id: test_id, status: "購入中")
   end
 
   def item_params
@@ -108,5 +80,7 @@ class ItemsController < ApplicationController
       :category_id
     )
   end
+    
 end
+
 
