d90b9dae6644b010034112fa1beaf9accc239a59
natsuki-tacica <sns.senyou.n@gmail.com>
2019-12-16 13:35:01 +0900

    add 商品削除の単体テスト

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe ItemsController, type: :controller do
+
+  describe 'DELITE #destroy' do
+  it "期待するビューに遷移するか？" do
+    get :index
+    expect(response).to render_template :index
+  end
+end
+
+end
