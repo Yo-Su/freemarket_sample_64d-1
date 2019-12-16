bbbadf2450675f7aa3d1f3ad7c23cdfa324f0947
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-12-16 17:14:42 +0900

    fix Sider修正

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -3,22 +3,24 @@ $(function(){
   //  商品詳細画面の画像を切り替える
   $('.content__item-data__photo__sub-images__sub-image').on('mouseover', function(e){
     e.preventDefault();
-
+    // 商品の画像一覧（ピックアップ画像、サブ画像）
+    var pickupImages = $('.content__item-data__photo__main-image__pickupimage')
+    var pickupSubImages = $('.content__item-data__photo__sub-images__sub-image')
     // 選択した(ピックアップしたい)画像のID
-    let pickupImageId = $(this).children().attr("data-sub-image-id")
+    var pickupImageId = $(this).children().attr("data-sub-image-id")
     // 詮索されたピックアップ画像、サブ画像
-    let pickupImage = $(`[data-image-id="${pickupImageId}"]`).parent()
-    let pickupSubImage = $(`[data-sub-image-id="${pickupImageId}"]`).parent()
+    var pickupImage = $(`[data-image-id="${pickupImageId}"]`).parent()
+    var pickupSubImage = $(`[data-sub-image-id="${pickupImageId}"]`).parent()
 
     // 全部の画像を非表示にする
-    $('.content__item-data__photo__main-image__pickupimage').each(function(index, element){
+    pickupImages.each(function(index, element){
       $(element).addClass("image-hidden")
     })
     // ピックアップしたい画像を表示する(非表示クラスを除去する)
     pickupImage.removeClass("image-hidden")
 
     // 全部の画像に透明度を設定する
-    $('.content__item-data__photo__sub-images__sub-image').each(function(index, element){
+    pickupSubImages.each(function(index, element){
       $(element).addClass("image-opacity")
     })
     // 選択したサブ画像の透明度を解除する(透明度クラスを除去する)
