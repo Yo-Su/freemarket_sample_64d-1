93ecdefe06ed18acdb2735729e5ee53466e179d3
wakuwakukatsu <katsu223desu@gmail.com>
2019-12-24 20:15:41 +0900

    fix sortを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
-  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
 end
